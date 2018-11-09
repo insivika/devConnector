d954ce4a3449fc166846e7ad58c3d0a6ad9dea37
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-11-08 23:00:07 -0800

    [WIP] added jtw authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys')
-
+const passport = require('passport');
 
 // Load User model
 const User = require('../../models/User');
@@ -99,5 +99,16 @@ router.post('/login', (req, res) => {
       })
 })
 
+// @route     GET api/users/current
+// @desc      Return Current User
+// @access    Private
+router.get('/current', passport.authenticate('jwt', {session: false }), 
+(req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+});
 
 module.exports = router;
