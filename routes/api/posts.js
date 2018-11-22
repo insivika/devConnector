2ee3330e8186b5905519a81a4370b0af3f4425ca
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-11-21 23:38:59 -0800

    [WIP] Implemented Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,6 +16,32 @@ router.get('/test', (req, res) => {
     res.json({msg: 'Posts works!'})
 });
 
+// @route     GET api/posts
+// @desc      Get posts
+// @access    Public
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({date: -1})
+        .then(posts => {
+            res.json(posts)
+        })
+        .catch(err => res.status(404).json({nopostsfound: 'No posts found'}))
+})
+
+// @route     GET api/posts/:id
+// @desc      Get post by id
+// @access    Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            res.json(post)
+        })
+        .catch(err => res.status(404).json({nopostfound: 'No post found with that ID'}))
+})
+
+
+
+
 // @route     POST api/posts
 // @desc      Create post
 // @access    Private
@@ -36,6 +62,25 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 
     });
     newPost.save().then(post => res.json(post));
+})
+
+// @route     DELETE api/posts/:id
+// @desc      Delete post by id
+// @access    Private
+router.delete('/:id', passport.authenticate('jwt',  {session: false}), (req, res) => {
+    Profile.findOne({user: req.user.id})
+            Post.findById(req.params.id)
+                .then(post => {
+                    if(post.user.toString() !== req.user.id){
+                        return res.status(404).json({ notauthorized: 'User not authorized'} )
+                    }
+
+                    // Delete
+                    post.remove().then(() => res.json({ success: true }))
+                })
+                .catch(err => {
+                    res.status(404).json({ postnotfound: 'No post found' });
+        })
 })
 
 module.exports = router;
