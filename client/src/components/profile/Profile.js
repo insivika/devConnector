e8dfb02ff789e26b34765b90c9fc305bd7a68246
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-12-22 18:37:44 -0800

    [WIP] Completed the profile header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -11,17 +11,55 @@ import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
 
-  componentDidMount(){
-    if(this.props.match.params.handle){
-        
-        this.props.getProfileByHandle(this.props.match.params.handle)
+  componentDidMount() {
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push('/not-found');
     }
   }
 
   render() {
+    const { profile, loading }= this.props.profile;
+
+    let profileContent;
+
+    if(profile === null || loading){
+      profileContent = <Spinner />
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back to Profiles
+              </Link>
+            </div>
+            <div className="col-md-6"></div>
+          </div>
+
+          <ProfileHeader profile={profile}/>
+          <ProfileAbout />
+          <ProfileCreds />
+          <ProfileGithub />
+
+        </div>
+      )
+    }
+
     return (
-      <div>
-        
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              {profileContent}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -31,10 +69,10 @@ class Profile extends Component {
 Profile.propTypes = {
   getProfileByHandle: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   profile: state.profile
-}) 
+});
 
-export default connect(mapStateToProps, { getProfileByHandle})(Profile);
+export default connect(mapStateToProps, { getProfileByHandle })(Profile);
